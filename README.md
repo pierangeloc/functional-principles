d7b7d112403bc6dc58b84a190592e9ac718fdd93
Pierangelo Cecchetto <pierangeloc@gmail.com>
2018-10-20 11:58:30 +0200

    monad transformers

---
+++ README.md
@@ -0,0 +1,21 @@
+# Functional Programming Principles
+
+A tour around some FP principles in Scala
+
+## To view the slides
+
+```
+sbt console
+```
+
+and then
+
+```
+scala> :load presentation.txt
+```
+
+and then `n <enter>` to proceed through the slides. `p <enter>` to go back.
+
+## Credit
+
+The slides are built using the excellent [REPLesent](https://github.com/marconilanna/REPLesent).
---
+++ src/main/scala/com/github/pierangeloc/fp/model.scala
@@ -0,0 +1,15 @@
+package com.github.pierangeloc.fp
+
+import java.util.UUID
+
+case class SubscriptionId(value: UUID) extends AnyVal
+case class TaskId(value: UUID) extends AnyVal
+case class UserId(value: String) extends AnyVal
+case class PaymentMeanId(value: UUID) extends AnyVal
+case class ServiceId(value: UUID) extends AnyVal
+case class TransactionId(value: UUID) extends AnyVal
+
+case class Subscription(id: SubscriptionId, taskId: TaskId)
+case class Task(id: TaskId, userId: UserId, paymentMeanId: PaymentMeanId, serviceId: ServiceId)
+case class TransactionRequest(paymentMeanId: PaymentMeanId, amount: BigDecimal, serviceId: ServiceId)
+case class Transaction(id: TransactionId, serviceId: ServiceId, paymentMeanId: PaymentMeanId, userId: UserId)
